5d6c9b8f2778c63f1cb1589b6ae98d1af839b7dc
yunaxie <bochenlong@163.com>
2018-05-18 13:56:00 +0800

    add

--- potato-sample/src/main/java/sample/BrpcServerDemo.java
+++ potato-sample/src/main/java/sample/BrpcServerDemo.java
@@ -2,10 +2,10 @@ package sample;
 
 import org.mengdadou.potato.websocket.PrpcConfig;
 import org.mengdadou.potato.websocket.codec.PrpcDecoder;
-import org.mengdadou.potato.websocket.message.PrpcMessage;
 import org.mengdadou.potato.websocket.codec.PrpcEncoder;
 import org.mengdadou.potato.websocket.core.PrpcServerConfigurator;
 import org.mengdadou.potato.websocket.core.PrpcWs;
+import org.mengdadou.potato.websocket.message.PrpcMessage;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
@@ -16,26 +16,26 @@ import java.util.concurrent.TimeUnit;
  */
 @ServerEndpoint(value = "/sample1/{uuid}/{subtype}", configurator = PrpcServerConfigurator.class, encoders = PrpcEncoder.class, decoders = PrpcDecoder.class)
 public class BrpcServerDemo {
-    private static PrpcWs prpcWs = new PrpcWs().setExecTimeout(30, TimeUnit.SECONDS);
-    
-    @OnOpen
-    public void onOpen(Session session) {
-        System.out.println("server get client ip*********" + session.getUserProperties().get(PrpcConfig.BRPC_CLIENT_IP));
-        prpcWs.onOpen(session);
-    }
-    
-    @OnMessage
-    public void onMessage(Session session, PrpcMessage message) {
-        prpcWs.onMessage(message, session);
-    }
-    
-    @OnClose
-    public void onClose(Session session, CloseReason reason) {
-        prpcWs.onClose(session, reason);
-    }
-    
-    @OnError
-    public void onError(Throwable t) {
-        prpcWs.onError(t);
-    }
+	private static PrpcWs prpcWs = new PrpcWs().setExecTimeout(30, TimeUnit.SECONDS);
+	
+	@OnOpen
+	public void onOpen(Session session) {
+		System.out.println(String.format("server get client ip********* %s", session.getUserProperties().get(PrpcConfig.BRPC_CLIENT_IP)));
+		prpcWs.onOpen(session);
+	}
+	
+	@OnMessage
+	public void onMessage(Session session, PrpcMessage message) {
+		prpcWs.onMessage(message, session);
+	}
+	
+	@OnClose
+	public void onClose(Session session, CloseReason reason) {
+		prpcWs.onClose(session, reason);
+	}
+	
+	@OnError
+	public void onError(Throwable t) {
+		prpcWs.onError(t);
+	}
 }
